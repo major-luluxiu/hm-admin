105eb4d115bf0bf80813b69826a90d775a5dd088
MajorTom <1477378328@qq.com>
2020-04-02 21:15:22 +0800

    登录页第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 全局引入element-ui
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+// ------------axios处理------------
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:3000'
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
---
+++ src/pages/Login.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="login">
+    <el-form ref="form" :model="form" :rules="rules" label-width="60px">
+      <el-form-item label="账号:" prop="username">
+        <el-input
+          v-model="form.username"
+          placeholder="请输入账号"
+          clearable
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码:" prop="password">
+        <el-input
+          v-model="form.password"
+          placeholder="请输入密码"
+          show-password
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '账号不能为空',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 5,
+            message: '长度在 3 到 5 个字符',
+            trigger: ['blur', 'change']
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '密码不能为空',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 6,
+            message: '长度在 3 到 6 个字位',
+            trigger: ['blur', 'change']
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 表单重置
+    reset() {
+      this.$refs.form.resetFields()
+    },
+    // 登录表单校验
+    async login() {
+      try {
+        // 发请求前先校验整个表单域
+        await this.$refs.form.validate()
+
+        const res = await this.$axios.post('/login', this.form)
+        console.log(res)
+      } catch (err) {}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-form {
+  width: 400px;
+  margin: 200px auto;
+  border: 1px solid #ccc;
+  padding: 20px;
+  .el-button:first-child {
+    margin-right: 80px;
+  }
+}
+</style>
